70013bec239759f45ddf130fab595a5b2af29251
maximforever <flufci@gmail.com>
2020-04-03 17:00:43 -0600

    add postbuild script

---
+++ public/precache-manifest.5bc96c8790d98e06af153620f45044ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77683ae691b2888d435bb9fbba9fe608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7bc43d44e70525f7bb1",
+    "url": "/static/css/main.2cb55fad.chunk.css"
+  },
+  {
+    "revision": "0078006811a335e8b759",
+    "url": "/static/js/2.24d82f15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24d82f15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bc43d44e70525f7bb1",
+    "url": "/static/js/main.e281613e.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
