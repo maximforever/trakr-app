cda12e26720cd4bb766c0bd8766ca85b42fe802f
maximforever <flufci@gmail.com>
2020-04-19 11:36:19 -0600

    add some sample welcome copy

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -4,7 +4,18 @@ import '../assets/stylesheets/Welcome.scss';
 function Welcome(){
   return(
     <div className="welcome-page card">
-      Y'er gonna sign in here.
+      <h1>Welcome to Trakr</h1>
+      <h3>Trakr helps you</h3>
+      <ul>
+        <li>stay mindful of your spending</li>
+        <li>prevent impulse purchases</li>
+        <li>find holes in your budget</li>
+      </ul>
+
+      <div>
+        <p>To get started, sign in with your Google account</p>
+        <p></p>
+      </div>
     </div>
   )
 }
