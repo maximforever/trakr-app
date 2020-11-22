72ba3e080e8e1307a751c4f0fe7c01be87464cb7
maximforever <flufci@gmail.com>
2020-11-21 17:27:56 -0700

    expand purchase stats

--- client/src/components/spendingStatistics.js
+++ client/src/components/spendingStatistics.js
@@ -15,7 +15,12 @@ export default function SpendingStatistics(props){
     <div className="card opaque">
       <h3>{getStatisticsTitle(props.category)}</h3>
       <div className="one-statistic">
-        <span className="name">Average expense</span> 
+        <span className="name">Total purchases</span> 
+        <span className="value">{props.expenses.length}</span>
+      </div>
+
+      <div className="one-statistic">
+        <span className="name">Average purchase</span> 
         <span className="value">${getAverageSpending(props.expenses)}</span>
       </div>
 
@@ -23,12 +28,17 @@ export default function SpendingStatistics(props){
         <span className="name">Average spending per day</span> 
         <span className="value">${getSpendingPerDay(props.expenses, props.daysThisMonth)}</span>
       </div>
+
+      <div className="one-statistic">
+        <span className="name">Average purchases per day</span> 
+        <span className="value">{getAverageOccurence(props.expenses, props.daysThisMonth)}</span>
+      </div>
     </div>
   )
 }
 
 function getStatisticsTitle(category){
-  return "Quick stats on " + (category === "all" ? " all spending" : ` ${category} spending`)
+  return "Quick stats on " + (category === "all" ? " all spending" : ` ${category}`)
 }
 
 function getAverageSpending(expenses){
@@ -49,6 +59,20 @@ function getSpendingPerDay(expenses, daysThisMonth){
   }
 
   return formatAmount(sumOfAllExpenses(expenses)/daysThisMonth);
+}
+
+function getAverageOccurence(expenses, daysThisMonth) {
+  // if this is a past month, we need to account for days in the past month
+  // if it's a current month, we count days till now
+
+  const currentDate = new Date();
+  const expenseDate = new Date(expenses[0].timestamp);
+
+  if(currentDate.getMonth() === expenseDate.getMonth() && currentDate.getYear() === expenseDate.getYear()){
+    return Math.floor(expenses.length/currentDate.getDate() * 10)/10;
+  }
+
+  return Math.floor(expenses.length/daysThisMonth * 10)/10;
 }
 
 function sumOfAllExpenses(expenses){
