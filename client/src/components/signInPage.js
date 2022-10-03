7cdb4068cf06bb7254f61e6321992d5fd8f62ae8
Max Pekarsky <pekarsky.maxim@gmail.com>
2022-10-03 09:54:01 -0400

    prettify sign in page html and css

--- client/src/components/signInPage.js
+++ client/src/components/signInPage.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import '../assets/stylesheets/sign-in-page.scss';
-import { GoogleLogin } from 'react-google-login';
+import React from "react";
+import "../assets/stylesheets/sign-in-page.scss";
+import { GoogleLogin } from "react-google-login";
 
-function SignInPage(){
-  return(
+function SignInPage() {
+  return (
     <div className="logged-out-page card">
       <h1>Welcome to Trakr</h1>
       <h3>Trakr helps you</h3>
@@ -15,22 +15,22 @@ function SignInPage(){
 
       <div>
         <p>To get started, sign in with your Google account</p>
-        <div className="button-wrapper">{ renderGoogleLoginButton() }</div>
+        <div className="button-wrapper">{renderGoogleLoginButton()}</div>
       </div>
     </div>
-  )
+  );
 }
 
 function renderGoogleLoginButton() {
   const successfulGoogleResponse = (response) => {
-    window.location.href = `/auth/google_oauth2/callback?token=${response.tokenId}`
-  }
+    window.location.href = `/auth/google_oauth2/callback?token=${response.tokenId}`;
+  };
 
   const failedGoogleResponse = (response) => {
     console.log(response);
-  }
+  };
 
-  return(
+  return (
     <GoogleLogin
       clientId="261179776021-aia4ltj1eq494nmfj9fl7foh28o9ckdu.apps.googleusercontent.com"
       buttonText="Login"
@@ -38,9 +38,9 @@ function renderGoogleLoginButton() {
       onFailure={failedGoogleResponse}
       isSignedIn={false}
       uxMode="popup"
-      cookiePolicy={'single_host_origin'}
+      cookiePolicy={"single_host_origin"}
     />
-  )
+  );
 }
 
 export default SignInPage;
